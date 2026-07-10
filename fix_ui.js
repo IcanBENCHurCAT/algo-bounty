846b45a9a55c36a1f24c813f3da2257676b2c9e8
IcanBENCHurCAT <garretparker@gmail.com>
2026-07-09 21:33:54 -0400

    Delete fix_ui.js

--- fix_ui.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// We need to restore the correct classes to DocsPageClient.tsx for typography.
-let code = fs.readFileSync('dashboard/src/app/docs/DocsPageClient.tsx', 'utf8');
-
-// The reviewer mentioned `prose-invert` missing, let's make sure it's there.
-if (!code.includes('prose-invert')) {
-  // It is actually in the code we see, but maybe it got overwritten by something else? Let's check what we have.
-}
